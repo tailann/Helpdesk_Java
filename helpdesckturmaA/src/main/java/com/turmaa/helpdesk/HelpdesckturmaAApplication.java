353a276afd13ad9e1f3dc8fe11169d3c950bf0c8
tailan <josetailansilva@gmail.com>
2025-09-08 14:58:29 -0300

    atualização

---
+++ helpdesckturmaA/src/main/java/com/turmaa/helpdesk/HelpdesckturmaAApplication.java
@@ -0,0 +1,13 @@
+package com.turmaa.helpdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpdesckturmaAApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpdesckturmaAApplication.class, args);
+	}
+
+}
---
+++ helpdesckturmaA/src/test/java/com/turmaa/helpdesk/HelpdesckturmaAApplicationTests.java
@@ -0,0 +1,13 @@
+package com.turmaa.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpdesckturmaAApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
